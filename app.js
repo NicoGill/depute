c1361f19b444ac6f9864ad0fbb31a12bf73d9c7e
Nicolas Gillium <nicogdevweb@gmail.com>
2023-08-16 13:08:14 +0200

    Init repo

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const turf = require("@turf/turf");
+const circos = require("./data/circonscriptions-legislatives.json");
+const { findCirco } = require("./modules/checkPoint");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  const coord = req.body;
+  const point = turf.point([coord.lat, coord.long]);
+  let result = null;
+
+  try {
+    circos.features.forEach((f) => {
+      let poly = turf.polygon(f.geometry.coordinates);
+      let find = turf.booleanPointInPolygon(point, poly);
+
+      if (find) {
+        result = f.properties.REF;
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
